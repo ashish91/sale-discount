edad7628cf1399c550b5951c60daf3e82287057c
Ashish Gaur <ashishgaurp91@gmail.com>
2023-09-21 15:20:03 +0530

    Remove not needed lines

--- driver.rb
+++ driver.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/ruby
-require 'pry'
 require File.join(Dir.pwd, 'bin/require_tree')
 
 
 Io::OutputFormatter.please_enter_text
 
 items_line = gets
-# items_line = 'milk,milk, bread,banana,bread,bread,bread,milk,apple'
 items = Io::InputFormatter.parse_items(items_line)
 
 sale_price_configs = Parser::SalePriceParser.new
