31ac86024561d2af0cd1b8b659bc012d2be6b857
Ashish Gaur <ashishgaurp91@gmail.com>
2023-09-21 15:07:47 +0530

    Use currency from the configuration file

--- driver.rb
+++ driver.rb
@@ -5,8 +5,8 @@ require File.join(Dir.pwd, 'bin/require_tree')
 
 Io::OutputFormatter.please_enter_text
 
-# items_line = gets
-items_line = 'milk,milk, bread,banana,bread,bread,bread,milk,apple'
+items_line = gets
+# items_line = 'milk,milk, bread,banana,bread,bread,bread,milk,apple'
 items = Io::InputFormatter.parse_items(items_line)
 
 sale_price_configs = Parser::SalePriceParser.new
@@ -14,6 +14,5 @@ sale_price_configs = Parser::SalePriceParser.new
 pricing = PricingStrategy::PricingCalculator.new(items_input: items, sale_price_configs: sale_price_configs)
 pricing.calculate_price!
 
-Io::OutputFormatter.price_report(pricing)
+Io::OutputFormatter.price_report(pricing, sale_price_configs.currency)
 
-binding.pry
--- io/output_formatter.rb
+++ io/output_formatter.rb
@@ -8,15 +8,15 @@ module Io
         puts "Please enter all the items purchased separated by a comma"
       end
 
-      def price_report(pricing)
+      def price_report(pricing, currency)
         puts "Item     Quantity      Price"
         puts "--------------------------------------"
 
         pricing.sold_items.each do |item_name, sold_item|
-          puts "#{item_name.capitalize}    #{sold_item.quantity}    $#{sold_item.sold_price.round(ROUNDING_PRICES)}"
+          puts "#{item_name.capitalize}    #{sold_item.quantity}    #{currency}#{sold_item.sold_price.round(ROUNDING_PRICES)}"
         end
-        puts "Total price: $#{pricing.total_price.round(ROUNDING_PRICES)}"
-        puts "You saved $#{pricing.discount.round(ROUNDING_PRICES)} today"
+        puts "Total price: #{currency}#{pricing.total_price.round(ROUNDING_PRICES)}"
+        puts "You saved #{currency}#{pricing.discount.round(ROUNDING_PRICES)} today"
       end
 
     end
